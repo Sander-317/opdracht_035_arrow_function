bdd5d29dc38026278764eb2be501335f6f93fee5
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-05-20 13:09:49 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+const fivePlusSeven = () => console.log(5 + 7);
+
+const myFunction = (a, b) => console.log(a + b);
+
+const addFive = a => console.log(a + 5);
+
+const createObject = () => console.log(({greeting: "hoi"}));
+
+ikRockArrowFunctions();
+fivePlusSeven();
+myFunction(2, 4);
+addFive(3);
+createObject();
